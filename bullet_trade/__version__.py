3ef5b9d1da9ecd574a1631e3ae7526679fd99619
brucelee <13911566027@163.com>
2025-12-21 00:38:27 +0800

    v0.5.3 Fixes #1

--- bullet_trade/__version__.py
+++ bullet_trade/__version__.py
@@ -1,6 +1,6 @@
 """版本信息"""
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 __author__ = 'BulletTrade Team'
 __email__ = 'groundlee@gmail.com'
 __url__ = 'https://github.com/BulletTrade/bullet-trade'
